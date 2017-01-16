d9eaf3819386078529b19485ebe2add2cca31603
hhogwarts <kaushik.iiita@gmail.com>
2017-01-17 01:37:24 +0530

    updated First commit

---
+++ http_server.js
@@ -0,0 +1,16 @@
+var http = require("http")
+var request = require("request");
+
+var requestData = "";
+request("http://www.google.com", function(error, response, body){
+    requestData = body;
+});
+
+var server = http.createServer(function(request, response){
+  response.writeHead(200, {
+    "content-type": "text/plain"
+  });
+  response.end(requestData);
+});
+
+server.listen(8888);
